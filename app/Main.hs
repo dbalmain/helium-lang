efc1d0a1e55a11a2205334e350d70e25c6e8b4c1
Dave Balmain <dbalmain@gmail.com>
2026-02-17 06:19:06 +1100

    Chapter 1.0: Get basic repl working

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main (main) where
 
+import System.Console.Haskeline
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Helium Chapter 1 - Type an expression (Ctrl-D to quit)"
+  runInputT defaultSettings repl
+
+repl :: InputT IO ()
+repl = do
+  minput <- getInputLine "h> "
+  case minput of
+    Nothing -> outputStrLn ""
+    Just input -> do
+      outputStrLn input
+      repl
