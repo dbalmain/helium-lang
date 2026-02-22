91f1283a06880e1954a272b22533cb961f2436ae
Dave Balmain <dbalmain@gmail.com>
2026-02-23 06:26:23 +1100

    Chapter 2.2b: Add mtl for error handling in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
+import Control.Monad.Except (MonadError (throwError), runExceptT)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Helium.Eval (eval)
 import Helium.Parser (parseExpr)
 import System.Console.Haskeline
@@ -9,17 +11,23 @@ main = do
   putStrLn "Helium Chapter 2 - Type an expression (Ctrl-D to quit)"
   runInputT defaultSettings repl
 
+liftWithPrefix :: (MonadError String m) => String -> Either String a -> m a
+liftWithPrefix prefix (Left err) = throwError (prefix <> err)
+liftWithPrefix _ (Right val) = pure val
+
+run :: (MonadError String m, MonadIO m) => String -> m Int
+run input = do
+  expr <- liftWithPrefix "Parser Error: " (parseExpr input)
+  liftIO $ print expr
+  liftWithPrefix "Runtime Error: " (eval mempty expr)
+
 repl :: InputT IO ()
 repl = do
   minput <- getInputLine "h> "
   case minput of
     Nothing -> outputStrLn ""
     Just input -> do
-      case parseExpr input of
-        Left err -> outputStrLn $ "Parse Error: " <> err
-        Right expr -> do
-          outputStrLn $ show expr
-          case eval mempty expr of
-            Left err -> outputStrLn $ "Runtime Error: " <> err
-            Right result -> outputStrLn $ show result
+      outcome <- runExceptT (run input)
+      either outputStrLn (outputStrLn . show) outcome
+
       repl
